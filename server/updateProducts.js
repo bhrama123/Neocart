498bdcf1aab8875299bb2aeedb0d3f81dfa60e78
BhramaK K <bhramakariyappa@gmail.com>
2026-07-11 20:33:02 +0530

    Fix product images

--- server/updateProducts.js
+++ server/updateProducts.js
@@ -7,32 +7,17 @@ const products = [
   ["iPhone 15", "iphone15.jpg"],
   ["Samsung Galaxy S24", "s24.jpg"],
   ["OnePlus 12", "oneplus12.jpg"],
-  ["Google Pixel 9", "pixel9.jpg"],
-
   ["MacBook Air M3", "macbookairm3.jpg"],
   ["Dell XPS 15", "dellxps15.jpg"],
-  ["HP Victus 15", "hpvictus15.jpg"],
-  ["Lenovo Legion 5", "lenovolegion5.jpg"],
-
-  ["Sony WH-1000XM5", "sonywh1000xm5.jpg"],
-  ["Apple AirPods Pro", "airpodspro.jpg"],
-  ["JBL Tune 760NC", "jbl760nc.jpg"],
-  ["boAt Rockerz 450", "boat450.jpg"],
-
-  ["Apple Watch Series 9", "applewatch9.jpg"],
-  ["Samsung Galaxy Watch 6", "galaxywatch6.jpg"],
-  ["Noise ColorFit Pro 5", "noisecolorfit5.jpg"],
-  ["Fire-Boltt Phoenix", "firebolttphoenix.jpg"],
-
-  ["Logitech MX Master 3S", "logitechmxmaster3s.jpg"],
-  ["Redragon K552 Keyboard", "redragonk552.jpg"],
-  ["Xiaomi Power Bank 20000mAh", "xiaomipowerbank.jpg"],
-  ["Logitech C920 Webcam", "logitechc920.jpg"],
+  ["HP Victus 15", "hpvictus.jpg"],
+  ["Sony WH-1000XM5", "sony.jpg"],
+  ["Apple AirPods Pro", "airpods.jpg"],
+  ["Apple Watch Series 9", "applewatch.jpg"],
 ];
 
 async function updateProducts() {
   try {
-await mongoose.connect(process.env.MONGO_URI);
+await mongoose.connect("mongodb://127.0.0.1:27017/ecommerce");
     console.log("MongoDB Connected");
 
     for (const [name, image] of products) {
