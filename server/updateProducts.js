5e77acb25afbc7d3cc93bda1b5aff696e5d30139
BhramaK K <bhramakariyappa@gmail.com>
2026-07-12 09:50:32 +0530

    Fix Logitech webcam  image

--- server/updateProducts.js
+++ server/updateProducts.js
@@ -7,33 +7,48 @@ const products = [
   ["iPhone 15", "iphone15.jpg"],
   ["Samsung Galaxy S24", "s24.jpg"],
   ["OnePlus 12", "oneplus12.jpg"],
-  ["MacBook Air M3", "macbookairm3.jpg"],
+  ["Google Pixel 9", "pixel19.jpg"],
+
+  ["Apple MacBook Air M3", "macbookairm3.jpg"],
   ["Dell XPS 15", "dellxps15.jpg"],
   ["HP Victus 15", "hpvictus.jpg"],
-  ["Sony WH-1000XM5", "sony.jpg"],
-  ["Apple AirPods Pro", "airpods.jpg"],
-  ["Apple Watch Series 9", "applewatch.jpg"],
+  ["Lenovo Legion 5", "lenovolegion5.jpg"],
+
+  ["Sony WH-1000XM5", "sonywh1000xm5.jpg"],
+  ["Apple AirPods Pro", "airpodspro.jpg"],
+  ["JBL Tune 770NC", "jbl760nc.jpg"],
+  ["boAt Rockerz 550", "boat450.jpg"],
+
+  ["Apple Watch Series 10", "applewatch9.jpg"],
+  ["Samsung Galaxy Watch 7", "galaxywatch6.jpg"],
+  ["Noise ColorFit Pro", "noisecolorfit5.jpg"],
+  ["Fire-Boltt Ninja", "firebolttphonix.jpg"],
+
+  ["Logitech MX Master 3S", "logitechmxmaster3s.jpg"],
+  ["Mechanical Keyboard", "redragonk552.jpg"],
+  ["Xiaomi Power Bank", "xiaomipowerbank.jpg"],
+  ["Logitech C920 Webcam", "logitechc920.jpg"],
 ];
 
 async function updateProducts() {
   try {
-await mongoose.connect("mongodb://127.0.0.1:27017/ecommerce");
+    await mongoose.connect(process.env.MONGO_URI);
     console.log("MongoDB Connected");
 
     for (const [name, image] of products) {
       const result = await Product.updateOne(
-        { name },
-        { $set: { image } }
+        { name: name },
+        { $set: { image: image } }
       );
 
       console.log(
-        `${name} -> ${result.modifiedCount ? "Updated" : "Not changed"}`
+        `${name} -> matched: ${result.matchedCount}, modified: ${result.modifiedCount}`
       );
     }
 
-    console.log("All products processed");
+    console.log("ALL IMAGES UPDATED");
   } catch (error) {
-    console.error("Error:", error);
+    console.error(error);
   } finally {
     await mongoose.disconnect();
   }
